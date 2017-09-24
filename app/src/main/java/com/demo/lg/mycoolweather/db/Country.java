b1d174ffc8e305bffe6aa2c81972dba81112a24d
lg <lg@163.com>
2017-09-24 09:13:37 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/demo/lg/mycoolweather/db/Country.java
@@ -0,0 +1,47 @@
+package com.demo.lg.mycoolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Administrator on 2017/9/24.
+ */
+
+public class Country extends DataSupport {
+
+    private int id;
+    private String countryName;
+    private int weatherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public int getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(int weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
